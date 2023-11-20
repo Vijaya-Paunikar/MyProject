b67a057b6bada3661298e07f43c4cf90ca446100
Vijaya-Paunikar <144651081+Vijaya-Paunikar@users.noreply.github.com>
2023-11-20 10:31:57 +0530

    Add files via upload

---
+++ ProjectSprint/java/com/example/ProjectSprintApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.otp", "com.example.controller", "com.example.messaging"})
+public class ProjectSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSprintApplication.class, args);
+	}
+
+}
